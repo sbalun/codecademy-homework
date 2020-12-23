1e5be9284175c998f8eb8f22bed5849c334b913e
Scott Balun <sbalun@gmail.com>
2020-12-22 21:06:49 -0500

    added example 4

--- list-comprehensions.py
+++ list-comprehensions.py
@@ -1,8 +1,16 @@
 # List Comprehension
 
+# Helper Methods
+def print_output_seperator(example_number):
+    print("#")
+    print("Example " + str(example_number))
+    print("#")
+
+
 #
 # Example 1
 #
+print_output_seperator(1)
 
 heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
 
@@ -18,10 +26,10 @@ This list comprehension:
 can_ride_coaster = [height for height in heights if height > 161]
 
 print(can_ride_coaster)
-
 #
 # Example 2
 #
+print_output_seperator(2)
 
 celsius = [0, 10, 15, 32, -5, 27, 3]
 
@@ -36,10 +44,11 @@ This list comprehension
 fahrenheit = [temp * 9/5 + 32 for temp in celsius]
 
 print(fahrenheit)
-
 #
 # Example 3
 #
+print_output_seperator(3)
+
 nums = [4, 8, 15, 16, 23, 42]
 
 """
@@ -54,4 +63,16 @@ The list comprehension that follows will:
 
 parity = [number % 2 for number in nums]
 
-parity
+print(parity)
+
+#
+# Example 4
+#
+print_output_seperator(4)
+
+# Problem: Create a new list named greetings that adds "Hello, " in front of each name in the list names.
+
+names = ["Elaine", "George", "Jerry", "Cosmo"]
+greetings = ["Hello, "+ name for name in names]
+
+print(greetings)
