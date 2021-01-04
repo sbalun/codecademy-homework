47de14580430f970fd4180aa8443c832865aac75
Scott Balun <sbalun@gmail.com>
2021-01-04 12:59:52 -0500

    completed 5th exercise, x_length_words

--- code-challenge-string-methods.py
+++ code-challenge-string-methods.py
@@ -120,11 +120,27 @@ print("This should return 'apple' : " + substring_between_letters("apple", "p", 
 #
 output_seperator()
 # -------------------
-#
+#    X Length
 # -------------------
+# x_length_words()
+# Create a function called x_length_words that takes a string named sentence and an integer named x as parameters. 
+# This function should return True if every word in sentence has a length greater than or equal to x.
 
+def x_length_words(sentence, x):
+    all_words_greater_or_equal_to_x = True
+    word_list = sentence.split()
 
+    for word in word_list:
+        if len(word) < x:
+            all_words_greater_or_equal_to_x = False
 
+    return all_words_greater_or_equal_to_x
+
+# Testing the function, x_length_words
+print("The output for x_length_words should be False: " + str(x_length_words("i like apples", 2)))
+# should print False
+print("The output for x_length_words should be True: " + str(x_length_words("he likes apples", 2)))
+# should print True
 #
 output_seperator()
 # -------------------
